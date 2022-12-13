d9eb6290c6e59deb74f51d9bec8fd3238d2c983d
Gharam-Sar <sargharam@gmail.com>
2022-12-13 13:39:15 +0200

    basic layout and displaying current tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from 'react';
 
-function App() {
+let todo = [
+  {    id: 1,    task: "first step",    done: true,  },
+  {    id: 2,    task: "first step",    done: false,  },
+  {    id: 3,    task: "first step",    done: true,  },
+  {    id: 4,    task: "first step",    done: false,  },
+];
+
+
+function ToDo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+<div className="myDiv" key={todo.id}>
+<h2>Task number: {todo.id}</h2>
+          <h2>task: {todo.task}</h2>
+          <h2 style={{color:todo.done ?'green':'red' }}>{todo.done ? '✔':'X'} </h2>
+          <hr />
+        </div>
+
   );
 }
-
-export default App;
+export default function MyApp() {
+  return (
+    <div>
+      <div className = "space" >
+      </div>
+        <div className = "tasks">
+    {todo.map(todo => {
+      return (
+        <ToDo
+todo=        {todo}
+      />
+      );
+    })}
+  </div>
+    </div>
+  
+  );
+}
