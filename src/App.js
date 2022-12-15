c35f982ad40ef6ed59390c2f6e5216323ac3e6bf
Gharam-Sar <sargharam@gmail.com>
2022-12-15 13:02:54 +0200

    adding new tasks is done and added local storage as well

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import "./App.css";
 import { useState } from 'react';
 
-let todo = [
-  {    id: 1,    task: "first step",    done: true,  },
-  {    id: 2,    task: "first step",    done: false,  },
-  {    id: 3,    task: "first step",    done: true,  },
-  {    id: 4,    task: "first step",    done: false,  },
-];
+let todo = [];
+let s=[];
+if(localStorage.length==0){localStorage.setItem('ids',0);
 
+localStorage.setItem('todo', JSON.stringify(s));
 
+}
+window.onload = (event) => {
+  MyApp();
+};
 function ToDo({ todo }) {
+
   return (
   
 <div className="myDiv" key={todo.id}>
 <h2>Task number: {todo.id}</h2>
           <h2>task: {todo.task}</h2>
-          <h2 style={{color:todo.done ?'green':'red' }}>{todo.done ? '✔':'X'} </h2>
+          <h2 style={{color:todo.done ?'green':'red' }}>{todo.done ? '✔':'X'} 
+          {/* onClick={update_task(todo.id)} */}
+          </h2>
           <hr />
         </div>
 
   );
 }
-export default function MyApp() {
+function Tasks() {
+  let displaytodo = JSON.parse(localStorage.getItem('todo'));
+  
   return (
-    <div>
-      <div className = "space" >
-      </div>
-        <div className = "tasks">
-    {todo.map(todo => {
+    <div className = "tasks">
+    {displaytodo.map(todo => {
       return (
         <ToDo
 todo=        {todo}
@@ -35,6 +39,50 @@ todo=        {todo}
       );
     })}
   </div>
+  );
+  window.location.reload(false);
+}
+
+function Add_task() {
+  const [newtask, settask] = useState("");
+  const storedtodo = JSON.parse(localStorage.getItem('todo'));
+  todo= storedtodo.slice(0)
+  const add_task = (event) => {
+    console.log("should dd");
+    let new_id=parseInt(localStorage.getItem('ids')) +1;
+   todo.push( {    id: new_id,    task: newtask,    done: false,  },);
+   localStorage.setItem('todo', JSON.stringify(todo));
+
+    localStorage.setItem('ids', new_id);
+    window.location.reload(false);
+  }
+  return (
+<div>
+          <table>
+            <tr>
+              <td> <label for="taskName">Task</label> &nbsp;</td>
+              <td><input type="text" id="taskName" name="task name" value={newtask}  onChange={(e) => settask(e.target.value)}
+       /></td>
+              <td>&emsp;&nbsp;
+          <button className="addButton" onClick={add_task} > Add </button>
+        </td>
+            </tr>
+          </table>
+</div>
+
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+    <div className="componant">  <Add_task/></div>
+     <hr></hr>
+    
+   <Tasks></Tasks>
+
+
+   
     </div>
   
   );
