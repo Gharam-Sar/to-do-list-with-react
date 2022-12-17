656c4be1355978cb45c183f9e93606e3815ac179
Gharam-Sar <sargharam@gmail.com>
2022-12-18 00:12:38 +0200

    now you can delete tasks and mark as done on undone

--- src/App.js
+++ src/App.js
@@ -1,89 +1,174 @@
 import "./App.css";
-import { useState } from 'react';
-
+import { useState } from "react";
+import trash from "./imgs/trash.png";
 let todo = [];
-let s=[];
-if(localStorage.length==0){localStorage.setItem('ids',0);
+let s = [];
+let searchtask = [];
+let searching = false;
+if (localStorage.length === 0) {
+  localStorage.setItem("ids", 0);
 
-localStorage.setItem('todo', JSON.stringify(s));
-
+  localStorage.setItem("todo", JSON.stringify(s));
 }
 window.onload = (event) => {
   MyApp();
 };
+
 function ToDo({ todo }) {
+ 
 
   return (
-  
-<div className="myDiv" key={todo.id}>
-<h2>Task number: {todo.id}</h2>
-          <h2>task: {todo.task}</h2>
-          <h2 style={{color:todo.done ?'green':'red' }}>{todo.done ? '✔':'X'} 
-          {/* onClick={update_task(todo.id)} */}
-          </h2>
-          <hr />
-        </div>
+    <div className="myDiv" key={todo.id}>
+      <h2>Task number: {todo.id}</h2>
+      <h2>task: {todo.task}</h2>
+      <h2 style={{ color: todo.done ? "green" : "red" }}>
+        <pp
+          onClick={() => {
+            let id = todo.id;
+            let displaytodo = JSON.parse(localStorage.getItem("todo"));
+            if (displaytodo[id - 1].done) displaytodo[id - 1].done = false;
+            else displaytodo[id - 1].done = true;
 
+            localStorage.setItem("todo", JSON.stringify(displaytodo));
+            window.location.reload(false);
+          }}
+        >
+          {" "}
+          {todo.done ? "✔" : "X"}{" "}
+        </pp>
+
+        <img
+          onClick={() => {
+            let id = todo.id;
+            let newtodo = [];
+            let ids = parseInt(localStorage.getItem("ids")) - 1;
+            let displaytodo = JSON.parse(localStorage.getItem("todo"));
+            newtodo = displaytodo.slice(0, id - 1);
+            console.log(newtodo);
+
+            for (let i = id - 1; i < ids; i++) {
+              console.log(displaytodo[i]);
+              let str = displaytodo[i + 1];
+              str.id = i + 1;
+              newtodo.push(str);
+            }
+            console.log(newtodo);
+
+            localStorage.setItem("todo", JSON.stringify(newtodo));
+
+            localStorage.setItem("ids", ids);
+            window.location.reload(false);
+          }}
+          src={trash}
+          style={{ height: "28px" }}
+        ></img>
+      </h2>
+      <h2></h2>
+      <hr />
+    </div>
   );
 }
 function Tasks() {
-  let displaytodo = JSON.parse(localStorage.getItem('todo'));
-  
+  let displaytodo = JSON.parse(localStorage.getItem("todo"));
+
   return (
-    <div className = "tasks">
-    {displaytodo.map(todo => {
-      return (
-        <ToDo
-todo=        {todo}
-      />
-      );
-    })}
-  </div>
+    <div className="tasks">
+      {displaytodo.map((todo) => {
+        return <ToDo todo={todo} />;
+      })}
+    </div>
   );
-  window.location.reload(false);
 }
 
-function Add_task() {
+// function SearchTasks() {
+
+//   return (
+//     <div className = "tasks">
+//     {displaytodo.map(todo => {
+//       return (
+//         <ToDo
+// todo=        {todo}
+//       />
+//       );
+//     })}
+//   </div>
+//   );
+// }
+
+function AddTask() {
   const [newtask, settask] = useState("");
-  const storedtodo = JSON.parse(localStorage.getItem('todo'));
-  todo= storedtodo.slice(0)
+  const storedtodo = JSON.parse(localStorage.getItem("todo"));
+  todo = storedtodo.slice(0);
   const add_task = (event) => {
     console.log("should dd");
-    let new_id=parseInt(localStorage.getItem('ids')) +1;
-   todo.push( {    id: new_id,    task: newtask,    done: false,  },);
-   localStorage.setItem('todo', JSON.stringify(todo));
+    let new_id = parseInt(localStorage.getItem("ids")) + 1;
+    todo.push({ id: new_id, task: newtask, done: false });
+    localStorage.setItem("todo", JSON.stringify(todo));
 
-    localStorage.setItem('ids', new_id);
+    localStorage.setItem("ids", new_id);
     window.location.reload(false);
-  }
+  };
   return (
-<div>
-          <table>
-            <tr>
-              <td> <label for="taskName">Task</label> &nbsp;</td>
-              <td><input type="text" id="taskName" name="task name" value={newtask}  onChange={(e) => settask(e.target.value)}
-       /></td>
-              <td>&emsp;&nbsp;
-          <button className="addButton" onClick={add_task} > Add </button>
-        </td>
-            </tr>
-          </table>
-</div>
-
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              {" "}
+              <label>Task</label> &nbsp;
+            </td>
+            <td>
+              <input
+                type="text"
+                id="taskName"
+                name="task name"
+                value={newtask}
+                onChange={(e) => settask(e.target.value)}
+              />
+            </td>
+            <td>
+              &emsp;&nbsp;
+              <button className="addButton" onClick={add_task}>
+                {" "}
+                Add{" "}
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
+function SearchBar() {
+  const [filterText, setFilterText] = useState("");
 
+  return (
+    <div className="componant" aria-roledescription="search">
+      <input
+        type="search"
+        className="searchBar"
+        placeholder="Search for a task"
+      />
+    </div>
+  );
+}
 export default function MyApp() {
   return (
     <div>
-    <div className="componant">  <Add_task/></div>
-     <hr></hr>
-    
-   <Tasks></Tasks>
+      <div className="componant">
+        {" "}
+        <AddTask />
+      </div>
+      <hr></hr>
 
-
-   
+      <div>
+        {" "}
+        <SearchBar />
+      </div>
+      <hr></hr>
+      <Tasks />
+      {/* {searching : <SearchTasks/> ?<Tasks/> }  */}
+      <hr></hr>
     </div>
-  
   );
 }
